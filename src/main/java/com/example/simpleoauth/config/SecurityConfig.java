da0cf5b29fb174dc0c0507f4f5c8f7022904c4b0
FeelingXD <wlals425315@gmail.com>
2024-02-27 20:55:32 +0900

    security : 리퀘스트 경로 재설정

--- src/main/java/com/example/simpleoauth/config/SecurityConfig.java
+++ src/main/java/com/example/simpleoauth/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.simpleoauth.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,21 +7,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  {
+public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain defaultFilter(HttpSecurity http) throws Exception{
-        http.csrf((csrf)->csrf.ignoringRequestMatchers("/**"));
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain defaultFilter(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/**"));
         http.httpBasic(AbstractHttpConfigurer::disable);
         http.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.headers(headersConfigurer-> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         // 허용
-        http.authorizeHttpRequests(request->
-                request.requestMatchers("/h2-console/**").permitAll()
-                );
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers("/users/**").permitAll() //users 의 login 만 허용
+        );
         return http.build();
     }
 }
