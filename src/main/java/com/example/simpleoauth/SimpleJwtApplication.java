d3364b98bc0195c83afb11fa8f422051cc14ad3c
FeelingXD <wlals425315@gmail.com>
2024-03-05 19:12:53 +0900

    refactor: naming -module, project

--- src/main/java/com/example/simpleoauth/SimpleJwtApplication.java
+++ src/main/java/com/example/simpleoauth/SimpleJwtApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleOauthApplication {
+public class SimpleJwtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleOauthApplication.class, args);
+		SpringApplication.run(SimpleJwtApplication.class, args);
 	}
 
 }
